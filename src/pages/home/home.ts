395439e42fe6204c8c20ee524b9062287b097b85
Emdaisy1 <hilborn_emmav@live.ca>
2017-02-20 16:48:08 -0500

    Working delete

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,20 +1,36 @@
 import { Component } from '@angular/core';
 
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 import {AngularFire, FirebaseListObservable} from 'angularfire2';
+
+//Import details page
+import { DetailsPage } from '../details/details';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  //Properties
   games: FirebaseListObservable<any>;
-
-  constructor(public navCtrl: NavController, af:AngularFire) {
+  //Constructor
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, af:AngularFire) {
     this.games = af.database.list('/games');
 
+  }
+
+  //Methods
+  addGame(){
+    this.navCtrl.push(DetailsPage);
+  }
+
+  editGame() {
+    this.navCtrl.push(DetailsPage);
+  }
+
+  deleteGame(game) {
+    this.games.remove(game);
   }
 
 }
